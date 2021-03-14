5785bc8487096de588106cf0b203915c4f06dbd8
Glenda <gsalas1@uco.edu>
2021-03-14 14:41:04 -0500

    added splitNoise(), updated NoiseRemover

--- model/INoiseRemover.java
+++ model/INoiseRemover.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-
 public interface INoiseRemover {
 
-    public void readNoise(ArrayList<String> csStmt);
+    public void readNoise(ArrayList<String> Noise);
+
     public ArrayList<String> removeNoise(ArrayList<String> csStmt);
 
 }
