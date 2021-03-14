1a5cc9c2f72b2f7889c26f1f43bde2280f96c308
Glenda <gsalas1@uco.edu>
2021-03-14 14:42:13 -0500

    added splitNoise(), added noiseRemover algorithm

--- model/IInput.java
+++ model/IInput.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
 public interface IInput {
-   
+
     public void transmitIn(String stringStmt);
+
     public void splitInput(String stringStmt);
+
+    public void splitNoise(String noiseStmt);
+
     public void printNoiseInput();
+
     public void setPriority(int num);
-    
+
 }
--- model/INoiseRemover.java
+++ model/INoiseRemover.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public interface INoiseRemover {
 
-    public void readNoise(ArrayList<String> Noise);
+    public void readNoise(ArrayList<String> noise);
 
     public ArrayList<String> removeNoise(ArrayList<String> csStmt);
 
